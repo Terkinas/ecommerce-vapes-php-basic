5e720d5497842f05f61453827287708667251dde
Terkinas <terkinas@gmail.com>
2022-08-04 23:25:12 +0300

    gss4

--- resources/views/livewire/age-confirmation.blade.php
+++ resources/views/livewire/age-confirmation.blade.php
@@ -14,11 +14,12 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">This site requires you to be over 18 years old?</h3>
+                    <p class="mb-5 text-xs font-normal text-gray-500 dark:text-gray-400">You must be 18 years of age or older to view page. Please verify your age to enter.</p>
                     <div class="w-full flex justify-between flex-nowrap">
-                        <button data-modal-toggle="popup-modal" type="submit" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-semibold rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                            Yes, I'm over 18 of age
+                        <button data-modal-toggle="popup-modal" type="submit" class="uppercase whitespace-nowrap text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-semibold rounded text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                            I am 18 or older
                         </button>
-                        <button data-modal-toggle="popup-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">I'm under 18</button>
+                        <button data-modal-toggle="popup-modal" type="button" class="uppercase text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded border border-gray-200 text-sm font-semibold px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">I am under 18</button>
                     </div>
                 </div>
             </form>
